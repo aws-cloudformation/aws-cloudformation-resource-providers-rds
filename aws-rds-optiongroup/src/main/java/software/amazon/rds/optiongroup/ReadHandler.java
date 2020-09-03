d1d11b12ce14ddb1d73d740126c4eb81234135a7
Oleg Sidorov <sidorovo@amazon.com>
2020-09-03 14:50:48 +0200

    Implement AWS::RDS::OptionGroup resource
    The implementation conforms to the public spec:
    https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-rds-optiongroup.html
    
    UpdateHandler handles Oracle APEX option groups in a non-straightforward
    way by preventing a version downgrade. I.e. if the handler detects a
    version downgrade, the version attribute would be silently restored to the
    current one. More details here:
    https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.Oracle.Options.html
    
    Signed-off-by: Oleg Sidorov <sidorovo@amazon.com>

---
+++ aws-rds-optiongroup/src/main/java/software/amazon/rds/optiongroup/ReadHandler.java
@@ -0,0 +1,51 @@
+package software.amazon.rds.optiongroup;
+
+import java.util.List;
+
+import software.amazon.awssdk.services.rds.RdsClient;
+import software.amazon.awssdk.services.rds.model.ListTagsForResourceRequest;
+import software.amazon.awssdk.services.rds.model.ListTagsForResourceResponse;
+import software.amazon.awssdk.services.rds.model.OptionGroup;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ProxyClient;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+
+public class ReadHandler extends BaseHandlerStd {
+    protected ProgressEvent<ResourceModel, CallbackContext> handleRequest(
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final ProxyClient<RdsClient> proxyClient,
+            final Logger logger) {
+
+        return proxy.initiate("rds::read-option-group", proxyClient, request.getDesiredResourceState(), callbackContext)
+                .translateToServiceRequest(Translator::describeOptionGroupsRequest)
+                .backoffDelay(BACKOFF_DELAY)
+                .makeServiceCall((describeRequest, proxyInvocation) -> proxyInvocation.injectCredentialsAndInvokeV2(
+                        describeRequest,
+                        proxyInvocation.client()::describeOptionGroups
+                ))
+                .handleError((describeRequest, exception, client, resourceModel, ctx) -> handleException(
+                        ProgressEvent.progress(resourceModel, ctx),
+                        exception
+                ))
+                .done((describeRequest, describeResponse, proxyInvocation, model, context) -> {
+                    final OptionGroup optionGroup = describeResponse.optionGroupsList().stream().findFirst().get();
+                    final List<OptionConfiguration> optionConfigurations = Translator.translateOptionConfigurationsFromSdk(optionGroup.options());
+                    final List<Tag> tags = listTagsSoftFailOnAccessDenied(proxyInvocation, optionGroup.optionGroupArn());
+                    return ProgressEvent.success(
+                            ResourceModel.builder()
+                                    .optionGroupName(optionGroup.optionGroupName())
+                                    .engineName(optionGroup.engineName())
+                                    .majorEngineVersion(optionGroup.majorEngineVersion())
+                                    .optionGroupDescription(optionGroup.optionGroupDescription())
+                                    .optionConfigurations(optionConfigurations)
+                                    .tags(tags)
+                                    .build(),
+                            context
+                    );
+                });
+    }
+}
---
+++ aws-rds-optiongroup/src/test/java/software/amazon/rds/optiongroup/CreateHandlerTest.java
@@ -0,0 +1,153 @@
+package software.amazon.rds.optiongroup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.security.InvalidParameterException;
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import software.amazon.awssdk.services.rds.RdsClient;
+import software.amazon.awssdk.services.rds.model.CreateOptionGroupRequest;
+import software.amazon.awssdk.services.rds.model.CreateOptionGroupResponse;
+import software.amazon.awssdk.services.rds.model.DescribeOptionGroupsRequest;
+import software.amazon.awssdk.services.rds.model.DescribeOptionGroupsResponse;
+import software.amazon.awssdk.services.rds.model.ListTagsForResourceRequest;
+import software.amazon.awssdk.services.rds.model.ListTagsForResourceResponse;
+import software.amazon.awssdk.services.rds.model.ModifyOptionGroupRequest;
+import software.amazon.awssdk.services.rds.model.OptionGroupAlreadyExistsException;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ProxyClient;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateHandlerTest extends AbstractTestBase {
+
+    @Mock
+    private AmazonWebServicesClientProxy proxy;
+
+    @Mock
+    private ProxyClient<RdsClient> proxyClient;
+
+    @Mock
+    RdsClient rdsClient;
+
+    private CreateHandler handler;
+
+    @BeforeEach
+    public void setup() {
+        handler = new CreateHandler();
+        proxy = new AmazonWebServicesClientProxy(logger, MOCK_CREDENTIALS, () -> Duration.ofSeconds(600).toMillis());
+        rdsClient = mock(RdsClient.class);
+        proxyClient = MOCK_PROXY(proxy, rdsClient);
+    }
+
+    @AfterEach
+    public void tear_down() {
+        verify(rdsClient, atLeastOnce()).serviceName();
+        verifyNoMoreInteractions(rdsClient);
+    }
+
+    @Test
+    public void handleRequest_SimpleSuccess() {
+        final CreateOptionGroupResponse createOptionGroupResponse = CreateOptionGroupResponse.builder().build();
+        when(proxyClient.client().createOptionGroup(any(CreateOptionGroupRequest.class))).thenReturn(createOptionGroupResponse);
+
+        final DescribeOptionGroupsResponse describeOptionGroupResponse = DescribeOptionGroupsResponse.builder().optionGroupsList(OPTION_GROUP_ACTIVE).build();
+        when(proxyClient.client().describeOptionGroups(any(DescribeOptionGroupsRequest.class))).thenReturn(describeOptionGroupResponse);
+
+        final ListTagsForResourceResponse listTagsForResourceResponse = ListTagsForResourceResponse.builder().build();
+        when(proxyClient.client().listTagsForResource(any(ListTagsForResourceRequest.class))).thenReturn(listTagsForResourceResponse);
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(RESOURCE_MODEL)
+                .logicalResourceIdentifier("option-group")
+                .desiredResourceTags(translateTagsToMap(TAG_SET))
+                .clientRequestToken("3b8cacab-1328-456f-a11f-64efb80ab51a")
+                .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+
+        verify(proxyClient.client()).createOptionGroup(any(CreateOptionGroupRequest.class));
+        verify(proxyClient.client()).modifyOptionGroup(any(ModifyOptionGroupRequest.class));
+        verify(proxyClient.client()).describeOptionGroups(any(DescribeOptionGroupsRequest.class));
+        verify(proxyClient.client()).listTagsForResource(any(ListTagsForResourceRequest.class));
+    }
+
+    @Test
+    public void handleRequest_SimpleSuccessAlreadyExists() {
+        when(proxyClient.client().createOptionGroup(any(CreateOptionGroupRequest.class))).thenThrow(
+                OptionGroupAlreadyExistsException.class
+        );
+
+        final DescribeOptionGroupsResponse describeOptionGroupResponse = DescribeOptionGroupsResponse.builder().optionGroupsList(OPTION_GROUP_ACTIVE).build();
+        when(proxyClient.client().describeOptionGroups(any(DescribeOptionGroupsRequest.class))).thenReturn(describeOptionGroupResponse);
+
+        final ListTagsForResourceResponse listTagsForResourceResponse = ListTagsForResourceResponse.builder().build();
+        when(proxyClient.client().listTagsForResource(any(ListTagsForResourceRequest.class))).thenReturn(listTagsForResourceResponse);
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(RESOURCE_MODEL)
+                .logicalResourceIdentifier("option-group")
+                .desiredResourceTags(translateTagsToMap(TAG_SET))
+                .clientRequestToken("3b8cacab-1328-456f-a11f-64efb80ab51a")
+                .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isNull();
+
+        verify(proxyClient.client()).createOptionGroup(any(CreateOptionGroupRequest.class));
+        verify(proxyClient.client()).modifyOptionGroup(any(ModifyOptionGroupRequest.class));
+        verify(proxyClient.client()).describeOptionGroups(any(DescribeOptionGroupsRequest.class));
+        verify(proxyClient.client()).listTagsForResource(any(ListTagsForResourceRequest.class));
+    }
+
+    @Test
+    public void handleRequest_SimpleException() {
+        when(proxyClient.client().createOptionGroup(any(CreateOptionGroupRequest.class)))
+                .thenThrow(InvalidParameterException.class);
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(RESOURCE_MODEL)
+                .desiredResourceTags(translateTagsToMap(TAG_SET))
+                .build();
+
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).isNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InternalFailure);
+
+        verify(proxyClient.client()).createOptionGroup(any(CreateOptionGroupRequest.class));
+    }
+}
