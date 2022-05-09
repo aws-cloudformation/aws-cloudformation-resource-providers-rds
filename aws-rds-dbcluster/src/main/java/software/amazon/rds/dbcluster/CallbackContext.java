432513a1af1d605a93f768f2165cb040d20d44fe
Oleg Sidorov <sidorovo@amazon.com>
2022-05-09 16:49:48 +0200

    [DBCluster] Soft fail tagging AccessDenied on create
    This commit changes the tagging soft fail algorithm. RDS resources
    implement tagging with a best-effort support: the handler code will
    attempt to create the resource with system tags only and then add the
    remaining tags separately. We are changing this approach to a 2-attempt
    run: initially the handler will attempt to create a resource with all
    tags, and if it fails, we will re-create the resource with system tags
    only (same as now) and attempt to invoke `AddTagsToResource` with the old
    soft-failing mechanism (silent failure).
    
    This allows RDS to handle cases where customer FAS policies provide an
    additional condition enforcing presence of particular tags in `Create*`
    requests.
    
    Signed-off-by: Oleg Sidorov <sidorovo@amazon.com>

--- aws-rds-dbcluster/src/main/java/software/amazon/rds/dbcluster/CallbackContext.java
+++ aws-rds-dbcluster/src/main/java/software/amazon/rds/dbcluster/CallbackContext.java
@@ -9,4 +9,5 @@ import software.amazon.cloudformation.proxy.StdCallbackContext;
 public class CallbackContext extends StdCallbackContext {
     private boolean modified;
     private boolean deleting;
+    private boolean createTagComplete;
 }
