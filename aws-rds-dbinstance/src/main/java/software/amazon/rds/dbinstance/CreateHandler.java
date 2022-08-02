64bfc5403bd9f242534d489144efb4836bc6afbe
Oleg Sidorov <sidorovo@amazon.com>
2022-08-02 11:19:09 +0200

    [DBInstance] Change stabilization logic on update (#251)
    This commit introduces a distinction between stabilization processes
    after create and update. The change is introduced for the sake of a
    complete logic parity with the historical implementation. In particular,
    the post-update stabilization is:
    - DBInstance status is `available`
    - DBParameterGroup status is `in-sync`
    - OptionGroup status is `in-sync`
    - if DBInstance is a member of a DBCluster:
      - DBClusterParameterGroup is `in-sync`
    
    Both stabilizers perform an additional assertion on DBInstance status.
    The assertion code performs an early process interruption if DBInstance
    enters one of the following statuses:
    - failed
    - incompatible-restore
    - incompatible-network
    - incompatible-parameters
    - inaccessible-encryption-credentials
    
    In this case the handler will throw a `CfnNotStabilizedException`.
    
    Signed-off-by: Oleg Sidorov <sidorovo@amazon.com>

--- aws-rds-dbinstance/src/main/java/software/amazon/rds/dbinstance/CreateHandler.java
+++ aws-rds-dbinstance/src/main/java/software/amazon/rds/dbinstance/CreateHandler.java
@@ -152,7 +152,7 @@ public class CreateHandler extends BaseHandlerStd {
                         proxyInvocation.client()::createDBInstance
                 ))
                 .stabilize((request, response, proxyInvocation, model, context) ->
-                        isDbInstanceStabilized(proxyInvocation, model))
+                        isDBInstanceStabilizedAfterCreate(proxyInvocation, model))
                 .handleError((request, exception, client, model, context) -> Commons.handleException(
                         ProgressEvent.progress(model, context),
                         exception,
@@ -179,7 +179,7 @@ public class CreateHandler extends BaseHandlerStd {
                         proxyInvocation.client()::createDBInstance
                 ))
                 .stabilize((request, response, proxyInvocation, model, context) ->
-                        isDbInstanceStabilized(proxyInvocation, model))
+                        isDBInstanceStabilizedAfterCreate(proxyInvocation, model))
                 .handleError((request, exception, client, model, context) -> Commons.handleException(
                         ProgressEvent.progress(model, context),
                         exception,
@@ -206,7 +206,7 @@ public class CreateHandler extends BaseHandlerStd {
                         proxyInvocation.client()::restoreDBInstanceFromDBSnapshot
                 ))
                 .stabilize((request, response, proxyInvocation, model, context) ->
-                        isDbInstanceStabilized(proxyInvocation, model))
+                        isDBInstanceStabilizedAfterCreate(proxyInvocation, model))
                 .handleError((request, exception, client, model, context) -> Commons.handleException(
                         ProgressEvent.progress(model, context),
                         exception,
@@ -233,7 +233,7 @@ public class CreateHandler extends BaseHandlerStd {
                         proxyInvocation.client()::restoreDBInstanceFromDBSnapshot
                 ))
                 .stabilize((request, response, proxyInvocation, model, context) ->
-                        isDbInstanceStabilized(proxyInvocation, model))
+                        isDBInstanceStabilizedAfterCreate(proxyInvocation, model))
                 .handleError((request, exception, client, model, context) -> Commons.handleException(
                         ProgressEvent.progress(model, context),
                         exception,
@@ -260,7 +260,7 @@ public class CreateHandler extends BaseHandlerStd {
                         proxyInvocation.client()::createDBInstanceReadReplica
                 ))
                 .stabilize((request, response, proxyInvocation, model, context) ->
-                        isDbInstanceStabilized(proxyInvocation, model))
+                        isDBInstanceStabilizedAfterCreate(proxyInvocation, model))
                 .handleError((request, exception, client, model, context) -> Commons.handleException(
                         ProgressEvent.progress(model, context),
                         exception,
