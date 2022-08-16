869b0416c3169a3304c05fc83b9827c44f3776f9
Tieg O'Sullivan <tiegaws@amazon.com>
2022-08-16 10:31:09 +0100

    updated nomenclature

--- aws-rds-dbparametergroup/src/main/java/software/amazon/rds/dbparametergroup/ParameterGrouper.java
+++ aws-rds-dbparametergroup/src/main/java/software/amazon/rds/dbparametergroup/ParameterGrouper.java
@@ -7,23 +7,23 @@ import java.util.List;
 import java.util.Map;
 
 public class ParameterGrouper implements AbstractGrouper<String, Parameter> {
-    private void fillPartitionWithIndependantParameters(Map<String, Parameter> parametersToUpdate, List<Parameter> group, int groupSize) {
+    private void fillPartitionWithIndependentParameters(Map<String, Parameter> parametersToUpdate, List<Parameter> partition, int partitionSize) {
         for (String key : parametersToUpdate.keySet()) {
-            if (group.size() == groupSize) {
+            if (partition.size() == partitionSize) {
                 return;
             }
-            group.add(parametersToUpdate.remove(key));
+            partition.add(parametersToUpdate.remove(key));
         }
     }
 
-    private void addNewPartitionWithDependantParameters(List<List<Parameter>> partitions, List<List<Parameter>> dependantParameters, int groupSize) {
+    private void addNewPartitionWithDependantParameters(List<List<Parameter>> partitions, List<List<Parameter>> dependantParameters, int partitionSize) {
         boolean updateOccurred = false;
         List<Parameter> newPartition = new ArrayList<>();
         do {
             updateOccurred = false;
             for (int i = 0; i < dependantParameters.size(); i++) {
                 List<Parameter> parametersToPartition = dependantParameters.get(i);
-                if (newPartition.size() + parametersToPartition.size() <= groupSize) {
+                if (newPartition.size() + parametersToPartition.size() <= partitionSize) {
                     newPartition.addAll(parametersToPartition);
                     dependantParameters.remove(parametersToPartition);
                     updateOccurred = true;
@@ -59,7 +59,7 @@ public class ParameterGrouper implements AbstractGrouper<String, Parameter> {
 
         while (numberOfParameterToPartition > 0) {
             addNewPartitionWithDependantParameters(partitions, dependantParameters, partitionSize);
-            fillPartitionWithIndependantParameters(parametersToUpdate, partitions.get(partitions.size()-1), partitionSize);
+            fillPartitionWithIndependentParameters(parametersToUpdate, partitions.get(partitions.size()-1), partitionSize);
             numberOfParameterToPartition -= partitions.get(partitions.size()-1).size();
         }
         return partitions;
