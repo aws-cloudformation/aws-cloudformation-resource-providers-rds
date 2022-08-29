d41a01200c02d4f0681d9a7d0b6f8663eaba50d9
Moataz Mohamed <azmoataz@amazon.com>
2022-08-29 17:24:16 +0100

    [DBClusterParameterGroup] changing stabilization from describe clusters to wait 5 minutes as per RDS documentation.

--- aws-rds-dbclusterparametergroup/src/main/java/software/amazon/rds/dbclusterparametergroup/CreateHandler.java
+++ aws-rds-dbclusterparametergroup/src/main/java/software/amazon/rds/dbclusterparametergroup/CreateHandler.java
@@ -39,8 +39,7 @@ public class CreateHandler extends BaseHandlerStd {
                 .stackTags(Tagging.translateTagsToSdk(request.getDesiredResourceTags()))
                 .resourceTags(Translator.translateTagsToSdk(request.getDesiredResourceState().getTags()))
                 .build();
-        logger.log("Here create");
-        logger.log(callbackContext.toString());
+
         return ProgressEvent.progress(request.getDesiredResourceState(), callbackContext)
                 .then(progress -> setDbClusterParameterGroupNameIfMissing(request, progress))
                 .then(progress -> Tagging.safeCreate(proxy, proxyClient, this::createDbClusterParameterGroup, progress, allTags))
