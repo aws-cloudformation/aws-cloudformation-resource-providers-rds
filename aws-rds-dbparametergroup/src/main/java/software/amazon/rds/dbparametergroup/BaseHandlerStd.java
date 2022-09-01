e2549aeb22d3483e6e6bcceb8e78059d99210748
Tieg O'Sullivan <tiegosullivanpsnl@gmail.com>
2022-09-01 14:25:41 +0100

    [DBParameterGroup] Add additional parameter dependencies (#295)
    * Update to dependency map to include correct parameter key 'character_set_server'
    
    * Update to dependency map to include new dependencies
    
    * fix compilation failures
    
    * Add parameter dependency
    
    * Remove unecessary parameter group dependency
    
    Co-authored-by: Tieg O'Sullivan <tiegaws@amazon.com>
    Co-authored-by: Valentin Shirshov <khebul@users.noreply.github.com>

--- aws-rds-dbparametergroup/src/main/java/software/amazon/rds/dbparametergroup/BaseHandlerStd.java
+++ aws-rds-dbparametergroup/src/main/java/software/amazon/rds/dbparametergroup/BaseHandlerStd.java
@@ -38,8 +38,10 @@ import software.amazon.rds.dbparametergroup.util.ParameterGrouper;
 
 public abstract class BaseHandlerStd extends BaseHandler<CallbackContext> {
     public static final List<Set<String>> DEPENDENCIES = ImmutableList.of(
-            ImmutableSet.of("collation_server", "character_set"),
-            ImmutableSet.of("gtid-mode", "enforce_gtid_consistency")
+            ImmutableSet.of("collation_server", "character_set_server"),
+            ImmutableSet.of("gtid-mode", "enforce_gtid_consistency"),
+            ImmutableSet.of("password_encryption", "rds.accepted_password_auth_method"),
+            ImmutableSet.of("ssl_max_protocol_version", "ssl_min_protocol_version")
     );
 
     protected static final ErrorRuleSet DEFAULT_DB_PARAMETER_GROUP_ERROR_RULE_SET = ErrorRuleSet
