70bc66649b352c78d632ef8a2372d99aaa6af980
Tieg O'Sullivan <tiegaws@amazon.com>
2022-10-29 13:20:00 +0100

    [DBClusterSnapshot] Initial commit from cfn init

---
+++ aws-rds-dbclustersnapshot/src/main/java/software/amazon/rds/dbclustersnapshot/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.rds.dbclustersnapshot;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-rds-dbclustersnapshot.json");
+    }
+}
