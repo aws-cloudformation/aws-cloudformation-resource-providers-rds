e874f8b936578ecea2ef5751443a5236ecae5df5
Tieg O'Sullivan <tiegaws@amazon.com>
2022-10-31 15:27:01 +0000

    [DBClusterSnapshot] Test suite conforming to other resources set up. Ready to write tests

--- aws-rds-dbclustersnapshot/src/main/java/software/amazon/rds/dbclustersnapshot/Configuration.java
+++ aws-rds-dbclustersnapshot/src/main/java/software/amazon/rds/dbclustersnapshot/Configuration.java
@@ -1,6 +1,14 @@
 package software.amazon.rds.dbclustersnapshot;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 class Configuration extends BaseConfiguration {
+
+    public JSONObject resourceSchemaJsonObject() {
+        return new JSONObject(
+                new JSONTokener(this.getClass().getClassLoader().getResourceAsStream(schemaFilename)));
+    }
 
     public Configuration() {
         super("aws-rds-dbclustersnapshot.json");
--- aws-rds-dbclustersnapshot/src/test/java/software/amazon/rds/dbclustersnapshot/ReadHandlerTest.java
+++ aws-rds-dbclustersnapshot/src/test/java/software/amazon/rds/dbclustersnapshot/ReadHandlerTest.java
@@ -1,10 +1,11 @@
 package software.amazon.rds.dbclustersnapshot;
 
 import java.time.Duration;
+
+import lombok.Getter;
 import software.amazon.awssdk.core.SdkClient;
+import software.amazon.awssdk.services.rds.RdsClient;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
-import software.amazon.cloudformation.proxy.OperationStatus;
-import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 import org.junit.jupiter.api.AfterEach;
@@ -13,30 +14,39 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.rds.test.common.core.HandlerName;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @ExtendWith(MockitoExtension.class)
-public class ReadHandlerTest extends AbstractTestBase {
+public class ReadHandlerTest extends AbstractHandlerTest {
 
     @Mock
+    @Getter
     private AmazonWebServicesClientProxy proxy;
 
     @Mock
-    private ProxyClient<SdkClient> proxyClient;
+    @Getter
+    private ProxyClient<RdsClient> proxyClient;
 
     @Mock
-    SdkClient sdkClient;
+    RdsClient rdsClient;
+
+    @Getter
+    private ReadHandler handler;
+
+    @Override
+    public HandlerName getHandlerName() {
+        return HandlerName.READ;
+    }
 
     @BeforeEach
     public void setup() {
         proxy = new AmazonWebServicesClientProxy(logger, MOCK_CREDENTIALS, () -> Duration.ofSeconds(600).toMillis());
-        sdkClient = mock(SdkClient.class);
-        proxyClient = MOCK_PROXY(proxy, sdkClient);
+        rdsClient = mock(RdsClient.class);
+        proxyClient = MOCK_PROXY(proxy, rdsClient);
     }
 
     @AfterEach
