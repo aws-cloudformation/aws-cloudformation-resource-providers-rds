513a8b50fd8af544d172a668d97d766b92bb2e47
Oleg Sidorov <sidorovo@amazon.com>
2023-01-31 12:49:57 +0100

    [DBClusterParameterGroup] Limit max filter size to 100
    This commit limits the parameter name filter size to 100 elements per
    request. RDS API would reject a request containing more than 100
    parameter names in the filter. The commit removes the recursive
    progressChain tailing and proposes a simplistic sequential current
    parameter accumulator buildup.
    
    Signed-off-by: Oleg Sidorov <sidorovo@amazon.com>

--- aws-rds-dbclusterparametergroup/src/test/java/software/amazon/rds/dbclusterparametergroup/TranslatorTest.java
+++ aws-rds-dbclusterparametergroup/src/test/java/software/amazon/rds/dbclusterparametergroup/TranslatorTest.java
@@ -1,18 +1,16 @@
 package software.amazon.rds.dbclusterparametergroup;
 
-import com.google.common.collect.ImmutableMap;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import software.amazon.awssdk.services.rds.model.DescribeDbClusterParametersRequest;
-import software.amazon.awssdk.services.rds.model.DescribeDbClustersRequest;
-import software.amazon.awssdk.services.rds.model.DescribeDbClustersResponse;
-import software.amazon.awssdk.services.rds.model.Filter;
-import software.amazon.cloudformation.exceptions.CfnInternalFailureException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.ImmutableList;
+import software.amazon.awssdk.services.rds.model.DescribeDbClusterParametersRequest;
+import software.amazon.awssdk.services.rds.model.Filter;
+import software.amazon.cloudformation.exceptions.CfnInternalFailureException;
 
 public class TranslatorTest {
 
@@ -20,9 +18,8 @@ public class TranslatorTest {
     public void describeDbClusterParametersFilteredRequest_throwIfParametersEmpty() {
         Assertions.assertThatThrownBy(() -> {
             Translator.describeDbClusterParametersFilteredRequest(
-                    ResourceModel.builder()
-                            .parameters(Collections.emptyMap())
-                            .build(),
+                    ResourceModel.builder().build(),
+                    Collections.emptyList(),
                     null
             );
         }).isInstanceOf(CfnInternalFailureException.class);
@@ -31,11 +28,10 @@ public class TranslatorTest {
     @Test
     public void describeDbClusterParametersFilteredRequest_shouldSetParameterNameFilters() {
         final DescribeDbClusterParametersRequest request = Translator.describeDbClusterParametersFilteredRequest(
-                ResourceModel.builder()
-                        .dBClusterParameterGroupName("DBClusterParameterGroup")
-                        .parameters(ImmutableMap.of("key1", "value1", "key2", "value2"))
-                        .build(),
-                null);
+                ResourceModel.builder().dBClusterParameterGroupName("DBClusterParameterGroup").build(),
+                ImmutableList.of("key1", "key2"),
+                null
+        );
         assertThat(request.filters().size()).isEqualTo(1);
         final Filter filter = request.filters().get(0);
         assertThat(filter.name()).isEqualTo("parameter-name");
