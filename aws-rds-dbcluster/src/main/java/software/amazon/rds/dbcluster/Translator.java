26b6ea33e9d62f3bddac0b93c390650a0b693da3
Valentin Shirshov <vshir@amazon.com>
2023-05-01 16:28:15 +0200

    [DBCluster] Align implementation with DBInstance

--- aws-rds-dbcluster/src/main/java/software/amazon/rds/dbcluster/Translator.java
+++ aws-rds-dbcluster/src/main/java/software/amazon/rds/dbcluster/Translator.java
@@ -308,7 +308,7 @@ public class Translator {
         if (null != desired.getManageMasterUserPassword()) {
             return desired.getManageMasterUserPassword();
         }
-        if (BooleanUtils.isTrue(previous.getManageMasterUserPassword()) && BooleanUtils.isNotTrue(desired.getManageMasterUserPassword())) {
+        if (BooleanUtils.isTrue(previous.getManageMasterUserPassword())) {
             return false;
         }
         return null;
