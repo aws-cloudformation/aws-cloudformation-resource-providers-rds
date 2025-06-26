291796b140c50f8188a5337557753e2d8f8eb9c8
Jeremy Chea <cheajer@amazon.com>
2025-06-26 16:49:46 +1000

    fix(DBParameterGroup): false drift on provided default value

--- aws-rds-dbparametergroup/src/test/java/software/amazon/rds/dbparametergroup/BaseHandlerStdTest.java
+++ aws-rds-dbparametergroup/src/test/java/software/amazon/rds/dbparametergroup/BaseHandlerStdTest.java
@@ -12,28 +12,21 @@ class BaseHandlerStdTest {
 
     @Test
     public void test_computeModifiedDBParameters() {
-        final Map<String, Parameter> engineDefaultParameters = ImmutableMap.of(
-                "param1", Parameter.builder().parameterName("param1").parameterValue("value1").build(),
-                "param2", Parameter.builder().parameterName("param2").parameterValue("value2").build(),
-                "param3", Parameter.builder().parameterName("param3").parameterValue("value3").build(),
-                "param4", Parameter.builder().parameterName("param4").build()
-        );
         final Map<String, Parameter> currentDBParameters = ImmutableMap.of(
-                "param1", Parameter.builder().parameterName("param1").parameterValue("value1").build(),
-                "param2", Parameter.builder().parameterName("param2").parameterValue("value1-2").build(),
-                "param4", Parameter.builder().parameterName("param4").build(),
-                "param5", Parameter.builder().parameterName("param5").parameterValue("value5").build()
+                "param1", Parameter.builder().parameterName("param1").parameterValue("value1").source("engine-default").build(),
+                "param2", Parameter.builder().parameterName("param2").parameterValue("value1-2").source("user").build(),
+                "param4", Parameter.builder().parameterName("param4").source("system").build(),
+                "param5", Parameter.builder().parameterName("param5").parameterValue("value5").source("user").build()
         );
 
         final Map<String, Parameter> modifiedParameters = BaseHandlerStd.computeModifiedDBParameters(
-                engineDefaultParameters,
                 currentDBParameters
         );
 
         Assertions.assertThat(modifiedParameters)
                 .isEqualTo(ImmutableMap.of(
-                        "param2", Parameter.builder().parameterName("param2").parameterValue("value1-2").build(),
-                        "param5", Parameter.builder().parameterName("param5").parameterValue("value5").build()
+                        "param2", Parameter.builder().parameterName("param2").parameterValue("value1-2").source("user").build(),
+                        "param5", Parameter.builder().parameterName("param5").parameterValue("value5").source("user").build()
                 ));
     }
 }
